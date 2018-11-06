a2126e566ca4b93618258f639a9d2c649273602f
Rıdvan Özgüvenir <ridvan.ozguvenir@ykteknoloji.com.tr>
2018-11-06 23:19:26 +0300

    tap to top.

--- app/src/main/java/com/ridvan/rxconverter/adapter/RxConverterAdapter.java
+++ app/src/main/java/com/ridvan/rxconverter/adapter/RxConverterAdapter.java
@@ -1,18 +1,28 @@
 package com.ridvan.rxconverter.adapter;
 
+import android.app.Activity;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.jakewharton.rxbinding2.widget.RxTextView;
 import com.ridvan.rxconverter.R;
+import com.ridvan.rxconverter.models.RxConverterResponse;
 import com.ridvan.rxconverter.models.RxCurrency;
+import com.ridvan.rxconverter.util.RxConverterConstants;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import io.reactivex.disposables.Disposable;
 
 /**
  * Created by ridvanozguvenir on 4.11.2018.
@@ -20,9 +30,39 @@ import java.util.List;
 public class RxConverterAdapter extends RecyclerView.Adapter<RxConverterAdapter.RxCurrencyViewHolder> {
 
     private List<RxCurrency> rxCurrencyList;
+    private Double baseCurrencyValue = 1.0;
 
-    public RxConverterAdapter(List<RxCurrency> currencyList) {
-        rxCurrencyList = currencyList;
+    public RxConverterAdapter() {
+        rxCurrencyList = new ArrayList<>();
+    }
+
+    public void creatOrUpdateCurrencyList(RxConverterResponse response) {
+        if (response != null) {
+            response.getRates().put(response.getBase(), baseCurrencyValue);
+            if (rxCurrencyList.size() > 0) {
+                this.updateCurrencyList(response.getRates());
+            } else {
+                rxCurrencyList.clear();
+                for (Map.Entry<String, Double> current : response.getRates().entrySet()) {
+                    if (RxConverterConstants.DEFAULT_CURRENCY.equals(current.getKey()))
+                        rxCurrencyList.add(0, new RxCurrency(current.getKey(), current.getValue()));
+                    rxCurrencyList.add(new RxCurrency(current.getKey(), current.getValue()));
+                }
+            }
+        }
+    }
+
+    public void updateCurrencyList(HashMap<String, Double> rates) {
+        for (String name : rates.keySet()) {
+            Double value = rates.get(name);
+
+            for (RxCurrency rxCurrency : rxCurrencyList) {
+                if (rxCurrency.getName().equals(name)) {
+                    rxCurrency.setValue(value);
+                    break;
+                }
+            }
+        }
     }
 
     @NonNull
@@ -41,7 +81,7 @@ public class RxConverterAdapter extends RecyclerView.Adapter<RxConverterAdapter.
         if (currency != null) {
             holder.bindTo(currency);
             holder.itemView.setOnClickListener(v -> swapSelectedItemToTop(position));
-            holder.editValue.setOnFocusChangeListener((v, hasFocus) -> focusValue());
+            //holder.editValue.setOnFocusChangeListener((v, hasFocus) -> focusValue(hasFocus, holder, position));
         }
     }
 
@@ -58,13 +98,45 @@ public class RxConverterAdapter extends RecyclerView.Adapter<RxConverterAdapter.
         }
     }
 
-    private void focusValue() {
+    private void focusValue(boolean hasFocus, RxCurrencyViewHolder viewHolder, int position) {
+        if (hasFocus) {
+            swapSelectedItemToTop(position);
+            viewHolder.rxEditValue = RxTextView.textChanges(viewHolder.editValue)
+                    .skip(1)
+                    .map(text -> text != null ? text : "")
+                    .map((CharSequence s) -> Double.parseDouble(s.toString()))
+                    .doOnNext(this::updateAllList)
+                    .subscribe();
+        } else {
+            if (viewHolder.rxEditValue != null) {
+                viewHolder.rxEditValue.dispose();
 
+                InputMethodManager imm = (InputMethodManager) viewHolder.itemView.getContext().getSystemService(Activity.INPUT_METHOD_SERVICE);
+                if (imm != null)
+                    imm.hideSoftInputFromWindow(viewHolder.editValue.getWindowToken(), 0);
+            }
+        }
+    }
+
+    private void updateAllList(Double newValue) {
+        for (RxCurrency rxCurrency : rxCurrencyList) {
+            rxCurrency.setValue(newValue * getValueByName(rxCurrency.getName()));
+        }
+    }
+
+    private Double getValueByName(String name) {
+        for (RxCurrency rxCurrency : rxCurrencyList) {
+            if (rxCurrency.getName().equals(name))
+                return rxCurrency.getValue();
+        }
+        return 0.00;
     }
 
     static class RxCurrencyViewHolder extends RecyclerView.ViewHolder {
         TextView currencyText;
         EditText editValue;
+
+        private Disposable rxEditValue;
 
         public RxCurrencyViewHolder(View itemView) {
             super(itemView);
--- app/src/main/java/com/ridvan/rxconverter/ui/MainActivity.java
+++ app/src/main/java/com/ridvan/rxconverter/ui/MainActivity.java
@@ -8,18 +8,16 @@ import android.support.v7.widget.RecyclerView;
 import com.ridvan.rxconverter.R;
 import com.ridvan.rxconverter.ServiceContext;
 import com.ridvan.rxconverter.adapter.RxConverterAdapter;
-import com.ridvan.rxconverter.models.RxCurrency;
 import com.ridvan.rxconverter.network.RxConverterService;
 import com.ridvan.rxconverter.util.RxConverterConstants;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 import io.reactivex.Observable;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.schedulers.Schedulers;
+
+import static com.ridvan.rxconverter.util.RxConverterConstants.DEFAULT_CURRENCY;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -31,33 +29,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         RecyclerView recyclerView = findViewById(R.id.my_recycler_view);
-        List<RxCurrency> list = new ArrayList<>();
-
         LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
         layoutManager.scrollToPosition(0);
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setHasFixedSize(true);
-        recyclerView.setAdapter(adapter);
+        adapter = new RxConverterAdapter();
 
         Observable.interval(RxConverterConstants.TIME_PERIOD, TimeUnit.MILLISECONDS)
-                .flatMap(n -> ServiceContext.instance.retrofit.create(RxConverterService.class).getLatestCurrencies(RxConverterConstants.DEFAULT_CURRENCY)
+                .flatMap(n -> ServiceContext.instance.retrofit.create(RxConverterService.class).getLatestCurrencies(DEFAULT_CURRENCY)
                         .flatMap(Observable::just)
                         .subscribeOn(Schedulers.io())
                         .observeOn(AndroidSchedulers.mainThread())
                         .onErrorResumeNext(observer -> {
                         })
                         .doOnNext(currencies -> {
-                            //TODO:success
-                            list.clear();
-                            for (Map.Entry<String, Double> current : currencies.getRates().entrySet())
-                                list.add(new RxCurrency(current.getKey(), current.getValue()));
-
-                            adapter = new RxConverterAdapter(list);
+                            adapter.creatOrUpdateCurrencyList(currencies);
                             recyclerView.setAdapter(adapter);
                         })
                         .doOnError(throwable -> {
-                            //TODO:fail
                         })
                 ).subscribe();
     }
+
 }
+
